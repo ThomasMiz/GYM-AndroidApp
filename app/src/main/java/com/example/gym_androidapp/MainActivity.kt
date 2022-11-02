e81a6cbf21aa4c142b10210212e71ed80ab559f5
ichayer <ichayer@itba.edu.ar>
2022-11-02 17:34:24 -0300

    Start developing initial activity

--- app/src/main/java/com/example/gym_androidapp/MainActivity.kt
+++ app/src/main/java/com/example/gym_androidapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.gym_androidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    var video : VideoView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        video = findViewById(R.id.video);
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.video;
+        val uri = Uri.parse(videoPath);
+        video!!.setVideoURI(uri);
+
+        val mediaController = MediaController(this);
+        video!!.setMediaController(mediaController);
+        mediaController.setAnchorView(video);
+        video!!.start();
+        video!!.setMediaController(null);
+        video!!.setOnPreparedListener { mp -> mp.isLooping = true }
     }
+
+    override fun onResume() {
+        super.onResume()
+        video!!.start()
+    }
+
 }
