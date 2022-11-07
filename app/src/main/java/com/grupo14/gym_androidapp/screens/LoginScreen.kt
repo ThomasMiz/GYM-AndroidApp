654e228c8f42a30704c9985bf95bae1f9c58cffb
ichayer <ichayer@itba.edu.ar>
2022-11-07 17:27:14 -0300

    Add navigation to RegisterScreen from LoginScreen

--- app/src/main/java/com/grupo14/gym_androidapp/screens/LoginScreen.kt
+++ app/src/main/java/com/grupo14/gym_androidapp/screens/LoginScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -131,7 +130,7 @@ fun LoginScreen(navController: NavHostController) {
                 color = MaterialTheme.colors.secondary,
             )
             TextButton(
-                onClick = {}
+                onClick = { navController.navigate("register") }
             ) {
                 Text(
                     "¡Registrate!",
