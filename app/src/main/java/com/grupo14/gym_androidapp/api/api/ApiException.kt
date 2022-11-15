3d1c57645997077395f2bed478e1525497626529
ichayer <ichayer@itba.edu.ar>
2022-11-14 21:02:16 -0300

    Update ApiException

--- app/src/main/java/com/grupo14/gym_androidapp/api/api/ApiException.kt
+++ app/src/main/java/com/grupo14/gym_androidapp/api/api/ApiException.kt
@@ -3,7 +3,7 @@ package com.grupo14.gym_androidapp.api.api
 import retrofit2.Response
 
 class ApiException(
-    response: Response<*>? = null,
+    val response: Response<*>? = null,
     cause: Throwable? = null
 ) : Exception(response?.message(), cause) {
 
