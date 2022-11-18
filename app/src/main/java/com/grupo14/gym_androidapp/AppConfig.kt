2487c9916ce483fc2303b71d544a1667e46904a1
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2022-11-17 23:49:36 -0300

    Update app/src/main/java/com/grupo14/gym_androidapp/AppConfig.kt

--- app/src/main/java/com/grupo14/gym_androidapp/AppConfig.kt
+++ app/src/main/java/com/grupo14/gym_androidapp/AppConfig.kt
@@ -2,7 +2,7 @@ package com.grupo14.gym_androidapp
 
 object AppConfig {
     val BASE_URL = "https://entrenagratis.es/"
-    val API_BASE_URL = "http://172.19.180.4:8080/api/"
+    val API_BASE_URL = "http://localhost:8080/api/"
 
     val INITIAL_AUTH_TOKEN: String? = null
 
